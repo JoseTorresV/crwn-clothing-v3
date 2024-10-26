01f66231a1c5d1353d3b469073a402577a1bf99b
José Torres <torresv.josemiguel@gmail.com>
2024-10-26 18:28:10 +0200

    Aggiunto routing, componente nav, logo e links per navigare

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import categories from "./data/categories.json";
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <div>Questa è un test</div>
+    </div>
+  );
+};
 
 const App = () => {
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 export default App;
